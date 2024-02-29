12d82233a410de956d0ef963909b3c9e1c4f3087
ttboma <ttboma@gmail.com>
2024-02-29 18:13:04 +0800

    feat: isValid

--- include/leetcode_cpp/solution.h
+++ include/leetcode_cpp/solution.h
@@ -1,18 +1,22 @@
-#pragma once
+#ifndef _SYCLEETCODESOL_H_
+#define _SYCLEETCODESOL_H_
 
 #include <iostream>
-#include <string>
-#include <vector>
 #include <map>
 #include <set>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
 /**
  * \brief LeetCode solutions provided by Shieh, Yueh-chang <ttboma@gmail.com>
  */
 class Solution {
-public:
+ public:
   int fib(int n);
   std::string addBinary(std::string a, std::string b);
+  bool isValid(std::string s);  // #20   Valid Parentheses
 };
+
+#endif  // _SYCLEETCODESOL_H_
