4c790620f8c386f10c8e5f87ceebc421c3751039
ttboma <ttboma@gmail.com>
2025-09-09 14:40:43 -0400

    refactor: apply custom clang format

--- bin/solution/main.cpp
+++ bin/solution/main.cpp
@@ -8,18 +8,18 @@ const auto HELP_MSG = "Usage: solution <COMMAND>\n\nCommands:";
 
 int main(int argc, char *argv[]) {
     po::options_description desc(HELP_MSG);
-    desc.add_options()("help", "Print this message or the help of the given subcommand(s)")(
-        "fib",
-        "[509. Fibonacci "
-        "Number](https://leetcode.com/problems/fibonacci-number/)")(
-        "isSubsequence",
-        "[392. Is "
-        "Subsequence](https://leetcode.com/problems/is-subsequence/description/"
-        "?envType=study-plan-v2&envId=top-interview-150)")(
-        "hasCycle",
-        "# [141. Linked List "
-        "Cycle](https://leetcode.com/problems/linked-list-cycle/description/"
-        "?envType=study-plan-v2&envId=top-interview-150)");
+
+    // NOTE:
+    //  Keep the options aligned for better readability, keep the clang-format off/on tags to avoid reformatting
+    //  by clang-format
+    // clang-format off
+    desc.add_options()
+        ("help"         , "Print this message or the help of the given subcommand(s)")
+        ("fib"          , "[509. Fibonacci Number](https://leetcode.com/problems/fibonacci-number/)")
+        ("isSubsequence", "[392. Is Subsequence](https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&envId=top-interview-150)")
+        ("hasCycle"     , "[141. Linked List Cycle](https://leetcode.com/problems/linked-list-cycle/description/?envType=study-plan-v2&envId=top-interview-150)")
+    ;
+    // clang-format on
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
